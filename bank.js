29d89844e1dc8163e412e78d5a6d2f7e6af589b4
Lucas Cunha <lucasgalvao260@gmail.com>
2024-02-23 16:18:57 -0300

    adding history transation

--- bank.js
+++ bank.js
@@ -1,25 +1,36 @@
 
-
+let history = []
 let balance = 1200
 let go = 0
 do {
-   
+    
     let option = prompt('Withdraw or Deposit:').toLowerCase();
     let valor = Number(prompt('Type the Valor:'));
+    history.push( { type: option, amount: valor } );
+    let amounts = history.map(transaction => transaction.amount);
+    let maxAmount = Math.max(...amounts);
      go = Number(prompt('Continue?\n1 - Continue\n2 - Stop'));
-
-    if (option === 'withdraw') {
+    
+    if( valor > 0){
+    if (option.toLocaleLowerCase() === 'withdraw') {
         if (valor <= balance) {
             balance -= valor;
         } else {
             console.log( 'Insufficient funds for withdrawal.' );
         }
-    } else if (option === 'deposit') {
+    } else if (option.toLocaleLowerCase() === 'deposit') {
         balance += valor;
     } else {
         console.log( 'Invalid option.' );
     }
     console.log( 'Current Balance:', balance );
+   
+    console.log(history);
+    console.log(maxAmount);
+
+    };
+    
+
 
 } while (go === 1);
 
