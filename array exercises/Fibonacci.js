bbed93bafd8a26a79e550cca7f53211141b5615e
Lucas Cunha <lucasgalvao260@gmail.com>
2024-02-26 18:55:18 -0300

    adding fibonacci exercise

---
+++ array exercises/Fibonacci.js
@@ -0,0 +1,16 @@
+
+    let number = Number(prompt('number'));
+    let array = [number-1 , number];
+    for(let i = 0 ; i <= 10 ; i++ ){
+        let nextNumber = array[i] + array[i + 1]
+        array.push(nextNumber)
+    };
+   
+    console.log(array)
+
+
+
+
+
+ 
+
